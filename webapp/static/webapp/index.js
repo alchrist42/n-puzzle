a50807a056b34d9e5f71dc952953abdae6849a33
Yellowjacket Manfryd <ymanfryd@va-c5.kzn.21-school.ru>
2022-06-04 17:55:42 +0300

    congratulation gifs

--- webapp/static/webapp/index.js
+++ webapp/static/webapp/index.js
@@ -8,9 +8,7 @@ const state = {
     disableButtons: false,
     solution: []
 }
-let bounceEaseOut = makeEaseOut(quad)
-
-/***bounce***/
+let bounceEaseOut = makeEaseOut(quad)//bounce
 
 function createItem(width, num) {
     const div = document.createElement('div')
@@ -37,13 +35,12 @@ function clearChildElements(element) {
     }
 }
 
-function moveElement(id) {
+function moveElementByClick(id) {
     const arr = state.currentCombination
     const distance = 500 / state.currentFieldSize
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].find(ind => ind === id) > 0) {
             const x = parseInt(arr[i].findIndex(ind => ind === id))
-            console.log(state.currentFieldSize)
             if (x < state.currentFieldSize - 1 && arr[i][x + 1] === 0)
                 move(id, distance, 'left', [i, x], [i, x + 1])
             else if (x > 0 && arr[i][x - 1] === 0)
@@ -52,9 +49,6 @@ function moveElement(id) {
                 move(id, distance, 'top', [i, x], [i + 1, x])
             else if (i > 0 && arr[i - 1][x] === 0)
                 move(id, distance, 'botoom', [i, x], [i - 1, x])
-            console.log(state.currentCombination.join(''), state.solution.join(''))
-            if (state.currentCombination.join('') === state.solution.join(''))
-                console.log('solved')
         }
     }
 }
@@ -67,7 +61,7 @@ function renderField() {
     state.currentCombination.map(item => item.map(it => arr.push(it)))
     for (let it of arr) {
         const item = createItem(itemSize, it)
-        item.addEventListener('click', () => moveElement(it))
+        item.addEventListener('click', () => moveElementByClick(it))
         field.appendChild(item)
     }
     return field
@@ -139,8 +133,7 @@ const getSolution = async (currentCombination) => {
         body: raw,
         redirect: 'follow'
     }
-    const res = await fetch('http://127.0.0.1:8000/solver/', requestOptions)
-    return res
+    return await fetch('http://127.0.0.1:8000/solver/', requestOptions)
 }
 
 function getDirection(current, destination) {
@@ -155,13 +148,28 @@ function getDirection(current, destination) {
     return null
 }
 
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function congratulateUser() {
+    PrintMessage('Congratulations!')
+    const random = randomIntFromInterval(1, 10)
+    const div = document.getElementById('rightSide')
+    const gif = document.createElement('img')
+    gif.className = 'congratsGif'
+    gif.src = `/static/webapp/assets/dance${random}.gif`;
+    div.appendChild(gif)
+}
+
 function swapElement(id, cur, dest) {
     state.currentCombination[dest[0]][dest[1]] = id
     state.currentCombination[cur[0]][cur[1]] = 0
     renderField(state.currentCombination)
     if (state.disableButtons)
         state.disableButtons = state.currentCombination.join('') !== state.solution.join('')
-    state.currentCombination.join('') === state.solution.join('') ? state.solution = [] : 0
+    if (state.currentCombination.join('') === state.solution.join(''))
+        congratulateUser()
 }
 
 function toggleLoader() {
@@ -185,15 +193,41 @@ function createParagraphs(data, element) {
 async function visualizeSolution(solution) {
     const div = document.getElementById('rightSide')
     clearChildElements(div)
-        const parsed = JSON.parse(solution)
-        const data = [
-            ['Coefficient         ', parsed.koof],
-            ['Checks              ', parsed.checks],
-            ['Cache length        ', parsed.cache_len],
-            ['Queue length        ', parsed.queue_len],
-            ['Time to get solution', parsed.spend_time],
-            ['Moves               ', parsed.moves?.length]]
-        createParagraphs(data, div)
+    const parsed = JSON.parse(solution)
+    const data = [
+        ['Coefficient         ', parsed.koof],
+        ['Checks              ', parsed.checks],
+        ['Cache length        ', parsed.cache_len],
+        ['Queue length        ', parsed.queue_len],
+        ['Time to get solution', parsed.spend_time],
+        ['Moves               ', parsed.moves?.length]]
+    createParagraphs(data, div)
+}
+
+function PrintMessage(message) {
+    state.disableButtons = false
+    const div = document.getElementById('rightSide')
+    clearChildElements(div)
+    const Message = document.createElement('p')
+    Message.innerHTML = message
+    Message.style.margin = '20px'
+    div.appendChild(Message)
+}
+
+function HandleServerSolution(text) {
+    visualizeSolution(text)
+    state.disableButtons = true
+    const moves = JSON.parse(text).moves
+    const distance = 500 / state.currentFieldSize
+    for (let i = 0; i < moves.length; i++) {
+        const id = moves[i][0]
+        const current = moves[i][1]
+        const destination = moves[i][2]
+        const direction = getDirection(current, destination)
+        state.currentTimers[i] = setTimeout(() => {
+            move(id, distance, direction, current, destination, i)
+        }, i * state.animationSpeed)
+    }
 }
 
 const listenerSolve = async function () {
@@ -201,23 +235,10 @@ const listenerSolve = async function () {
         toggleLoader()
         const solution = await getSolution(state.currentCombination)
         const text = await solution.text()
-        console.log(solution['error'])
-        if (solution.status === 200) {
-            visualizeSolution(text)
-            state.disableButtons = true
-            const moves = JSON.parse(text).moves
-            const distance = 500 / state.currentFieldSize
-            for (let i = 0; i < moves.length; i++) {
-                const id = moves[i][0]
-                const current = moves[i][1]
-                const destination = moves[i][2]
-                const direction = getDirection(current, destination)
-                state.currentTimers[i] = setTimeout(() => {
-                    move(id, distance, direction, current, destination, i)
-                }, i * state.animationSpeed)
-            }
-        }
-
+        if (solution.status === 200)
+            HandleServerSolution(text)
+        else
+            PrintMessage("Couldn't solve this puzzle\n Can you?")
     }
 }
 
