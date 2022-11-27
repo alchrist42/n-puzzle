ef84f63761c5efc98b1a83add84e795f894b4ca8
ymanfryd <ymanfryd@student.21-school.ru>
2022-11-27 14:14:58 +0300

    adding react

---
+++ frontend/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include(('webapp.urls', "webapp"), namespace='webapp')),
+    path('', include('frontend.urls')),
+]
